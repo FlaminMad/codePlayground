a71ee07cc4526e116de739a45a8c531cd88893e6
Alex <alex.leech@talktalk.net>
2018-04-25 22:24:49 +0100

    Upload of assignment 3 and general tidy up

---
+++ Assignments/Assignment 3/AssignmentThree.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Alexander David Leech
+@date:   Wed Apr 25 22:10:17 2018
+@rev:    1
+@lang:   Python 2.7
+@deps:   None
+@desc:   Assignment three
+"""
+
+# Assignment 3
+#
+#   Using the main file, create a function that will complete each of the below
+#   'mini' tasks. I would like the main function to be formatted as below:
+#   main():
+#       partA()
+#       time.sleep(5)
+#       partB()
+#       time.sleep(5)
+#       partC()
+#       time.sleep(5)
+#       etc...
+#
+# A.  Tell me the current date/time
+#
+#
+# B.  Output the current python version being used (Hint: import 'sys')
+#
+#
+# C.  When i type my first and last name, say it back to me in the format:
+#     lastname, firstname
+#
+#
+# D.  Tell me what items 1 and 4 of this list are:
+#     holidayPack = ["clothes","shoes","sunglasses","factor 50","tickets"]
+#
+# 
+# E.  When I type a letter, tell me if the letter I typed is a vowel or not
+#
+#
+# F.  Optimise the code I asked you to write in the main file if you think
+#     it can be done
+##
+
+# There are a total of 6 marks avaliable, with a bonus 5 for
+#  optimised code.
+#
+# Good luck
+#     And remember, when the fun stops, stop!
+
+print("Open source, open mind")
+
